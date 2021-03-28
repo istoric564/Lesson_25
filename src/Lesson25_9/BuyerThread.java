de0a957737d24054fa3b56ee41336797d4d2a135
istoric564 <mrdizzy38@gmail.com>
2021-03-28 16:53:51 +0700

    java.util.concurrent fix

--- src/Lesson25_9/BuyerThread.java
+++ src/Lesson25_9/BuyerThread.java
@@ -1,12 +1,13 @@
 package Lesson25_9;
 
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 
 public class BuyerThread implements Runnable {
 
-    private final Queue<CashBox> cashBoxes;
+    private final BlockingQueue<CashBox> cashBoxes;
 
-    public BuyerThread(Queue<CashBox> cashBoxes) {
+    public BuyerThread(BlockingQueue<CashBox> cashBoxes) {
         this.cashBoxes = cashBoxes;
     }
 
@@ -14,24 +15,29 @@ public class BuyerThread implements Runnable {
     public void run() {
 
         try {
-            synchronized (cashBoxes) {
-                while (true) {
-                    if (!cashBoxes.isEmpty()) {
-                        CashBox cashBox = cashBoxes.remove();
-                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashBox);
-
-                        cashBoxes.wait(10L);
-
-                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashBox);
-                        cashBoxes.add(cashBox);
-                        cashBoxes.notifyAll();
-                        break;
-                    } else {
-                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
-                        cashBoxes.wait();
-                    }
-                }
-            }
+            CashBox cashBox = cashBoxes.take();
+            System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashBox);
+            Thread.sleep(5L);
+            System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashBox);
+            cashBoxes.add(cashBox);
+//            synchronized (cashBoxes) {
+//                while (true) {
+//                    if (!cashBoxes.isEmpty()) {
+//                        CashBox cashBox = cashBoxes.remove();
+//                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashBox);
+//
+//                        cashBoxes.wait(10L);
+//
+//                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashBox);
+//                        cashBoxes.add(cashBox);
+//                        cashBoxes.notifyAll();
+//                        break;
+//                    } else {
+//                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
+//                        cashBoxes.wait();
+//                    }
+//                }
+//            }
 
 
         } catch (InterruptedException e) {
--- src/Lesson25_9/CashBoxRunner.java
+++ src/Lesson25_9/CashBoxRunner.java
@@ -3,12 +3,14 @@ package Lesson25_9;
 import java.util.ArrayDeque;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CashBoxRunner {
     public static void main(String[] args) throws InterruptedException {
-        Queue<CashBox> cashBoxes = new ArrayDeque<>(List.of(new CashBox(), new CashBox()));
+        BlockingQueue<CashBox> cashBoxes = new ArrayBlockingQueue<>(2, true, List.of(new CashBox(), new CashBox()));
 
         List<Thread> threads = Stream.of(
                 new BuyerThread(cashBoxes),
